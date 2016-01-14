f4d56051a8375fd33a7aa4b21c23bc625e17124d
in-the-box <chiaki.jpg@gmail.com>
2016-01-15 05:13:36 +0900

    2nd commit

--- link.sh
+++ link.sh
@@ -4,6 +4,8 @@ ln -sf ~/.dotfiles/.atom         ~/.atom
 ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/.bashrc       ~/.bashrc
 ln -sf ~/.dotfiles/.nanorc       ~/.nanorc
+ln -sf ~/.dotfiles/.gitconfig    ~/.gitconfig
+ln -sf ~/.dotfiles/.gitignore    ~/.gitignore
 
 
 source ~/.dotfiles/.bash_profile
