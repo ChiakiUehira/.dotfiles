f7b69d15e47a11a59457764c5c929e55a7a1cbee
haco <haco@in-the-haco.local>
2016-04-30 11:20:33 +0900

    fix

---
+++ functions/cd.fish
@@ -0,0 +1,4 @@
+function cd
+     builtin cd $argv;pwd;ls;
+end
+
--- link.sh
+++ link.sh
@@ -6,11 +6,13 @@ ln -sf ~/.dotfiles/.bashrc       ~/.bashrc
 ln -sf ~/.dotfiles/.nanorc       ~/.nanorc
 ln -sf ~/.dotfiles/.gitconfig    ~/.gitconfig
 ln -sf ~/.dotfiles/.gitignore    ~/.gitignore
-ln -sf ~/.dotfiles/.shell   	 ~/.shell
+ln -sf ~/.dotfiles/.shell     	 ~/.shell
 ln -sf ~/.dotfiles/config.fish 	 ~/.config/fish/config.fish
-ln -sf ~/.dotfiles/functions/*  ~/.config/fish/functions/
-ln -sf ~/.dotfiles/.tmux.conf    ~/.tmux.conf
-ln -sf ~/.dotfiles/.vimrc	 ~/.vimrc
+ln -sf ~/.dotfiles/functions/*   ~/.config/fish/functions/
+ln -sf ~/.dotfiles/.tmux.conf  	 ~/.tmux.conf
+ln -sf ~/.dotfiles/.vimrc			  	~/.vimrc
+
+
 source ~/.bash_profile
 source ~/.bashrc
 
