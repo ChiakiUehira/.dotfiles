5c4d1cf987fe578c89517c8da29ee9f3ea7a6a5c
haco <haco@in-the-haco.local>
2016-05-03 00:28:19 +0900

    fuck

--- link.sh
+++ link.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
+#!/bin/sh 
 
-ln -sf ~/.dotfiles/.atom         ~/.atom
 ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/.bashrc       ~/.bashrc
 ln -sf ~/.dotfiles/.nanorc       ~/.nanorc
