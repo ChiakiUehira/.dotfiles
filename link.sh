484362ef40cf10199522e0c2f253f918a62db366
chiaki <chiaki@in-the-box.local>
2016-06-05 08:15:26 +0900

    add: styles.less

--- link.sh
+++ link.sh
@@ -1,16 +1,16 @@
-#!/bin/sh 
+#!/bin/sh
 
 ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/.bashrc       ~/.bashrc
 ln -sf ~/.dotfiles/.nanorc       ~/.nanorc
 ln -sf ~/.dotfiles/.gitconfig    ~/.gitconfig
 ln -sf ~/.dotfiles/.gitignore    ~/.gitignore
-ln -sf ~/.dotfiles/.shell     	 ~/.shell
-ln -sf ~/.dotfiles/config.fish 	 ~/.config/fish/config.fish
+ln -sf ~/.dotfiles/.shell        ~/.shell
+ln -sf ~/.dotfiles/config.fish   ~/.config/fish/config.fish
 ln -sf ~/.dotfiles/functions/*   ~/.config/fish/functions/
-ln -sf ~/.dotfiles/.tmux.conf  	 ~/.tmux.conf
-ln -sf ~/.dotfiles/.vimrc			  	~/.vimrc
-
+ln -sf ~/.dotfiles/.tmux.conf    ~/.tmux.conf
+ln -sf ~/.dotfiles/.vimrc        ~/.vimrc
+ln -sf ~/.dotfiles/styles.less   ~/.atom/styles.less
 
 source ~/.bash_profile
 source ~/.bashrc
