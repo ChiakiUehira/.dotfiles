078a21538196fc28328fb4cec7372f2f4da743f5
haco <haco@in-the-haco.local>
2016-06-05 14:12:19 +0900

    fix

--- link.sh
+++ link.sh
@@ -5,7 +5,6 @@ ln -sf ~/.dotfiles/.bashrc       ~/.bashrc
 ln -sf ~/.dotfiles/.nanorc       ~/.nanorc
 ln -sf ~/.dotfiles/.gitconfig    ~/.gitconfig
 ln -sf ~/.dotfiles/.gitignore    ~/.gitignore
-ln -sf ~/.dotfiles/.shell        ~/.shell
 ln -sf ~/.dotfiles/config.fish   ~/.config/fish/config.fish
 ln -sf ~/.dotfiles/functions/*   ~/.config/fish/functions/
 ln -sf ~/.dotfiles/.tmux.conf    ~/.tmux.conf
