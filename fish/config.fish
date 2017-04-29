d1d1f9f3ad0b9cebe83d0d563c094c56778ee19f
in-the-box <hacoco345@gmail.com>
2017-04-29 14:46:14 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 
 set -gx FISH_ALIASES "~/.config/fish"
 set -gx PATH ~/.nodebrew/current/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
-set -gx GOPATH ~/go
+set -gx GOPATH ~/.go
 
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
