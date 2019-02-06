143119abd1cb58c9fe763ffbc08d2f0db84d2ac4
Chiaki Uehira <me@uhck.on>
2019-02-06 20:20:22 +0900

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 
 set -gx FISH_ALIASES "~/.config/fish"
 set -gx GOPATH ~/.go
-set -gx PATH ~/.nodebrew/current/bin /usr/local/php5/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin ~/.go/bin
+set -gx PATH ~/.nodebrew/current/bin /usr/local/php5/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin ~/.go/bin ~/.composer/vendor/bin
 
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
@@ -12,3 +12,6 @@ source ~/.config/fish/aliases.fish
 
 
 
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/haco/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/haco/google-cloud-sdk/path.fish.inc'; else; . '/Users/haco/google-cloud-sdk/path.fish.inc'; end; end
